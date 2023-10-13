8dfca37a35465b8dfe4359ab8f20a8f7723f4f10
jared <jaredcrace@gmail.com>
2023-10-12 20:20:05 -0500

    adding ebay/slack example

---
+++ examples/autogen_ebay_slack/demo.py
@@ -0,0 +1,53 @@
+import autogen
+
+# load config list from json file
+config_list = autogen.config_list_from_json(env_or_file="OAI_CONFIG_LIST.json")
+
+# config for autogen's enhanced inference API which is compatible with OpenAI API
+llm_config={
+    "request_timeout": 600,
+    "seed": 50,                     # for caching and reproducibility
+    "config_list": config_list,     # which models to use
+    "temperature": 0,               # for sampling
+}
+
+# create an AssistantAgent instance named "assistant"
+agent_assistant = autogen.AssistantAgent(
+    name="agent_assistant",
+    llm_config=llm_config,
+)
+
+# create a UserProxyAgent that acts on the humans behalf 
+agent_proxy = autogen.UserProxyAgent(
+    name="agent_proxy",
+    human_input_mode="NEVER",           # NEVER, TERMINATE, or ALWAYS 
+                                            # TERMINATE - human input needed when assistant sends TERMINATE 
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "agent_output",     # path for file output of program
+        "use_docker": False,            # True or image name like "python:3" to use docker image
+    },
+    llm_config=llm_config,
+    system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
+                      Otherwise, reply CONTINUE, or the reason why the task is not solved yet."""
+)
+
+
+agent_proxy.initiate_chat(
+    agent_assistant,
+    message="""I'd like for you to create a python script that meets the following requirements:
+    - uses the selenium python package
+    - uses the presence_of_element_located method from selenium
+    - does not use the method find_element_by_css_selector
+    - searches iphone 14 at ebay
+    - finds all the prices by looking for the class "s-item__price"
+    - prints to the screen all the prices it finds 
+    - check the length of the text of the price before printing or sending to slack, just skip if the length is less than 1
+    - sends all the prices to a slack channel 
+    - the slack url to use is 'https://hooks.slack.com/services/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'
+    """,
+)
+
+
+
---
+++ examples/autogen_ebay_slack/ebay_price_scraper_working.py
@@ -0,0 +1,45 @@
+# filename: ebay_price_scraper.py
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import time
+
+def send_to_slack(text):
+    webhook_url = 'https://hooks.slack.com/services/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'
+    slack_data = {'text': text}
+    response = requests.post(
+        webhook_url, json=slack_data,
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+driver = webdriver.Chrome()  # or webdriver.Chrome(), depending on your browser
+
+driver.get("https://www.ebay.com")
+
+search_box = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "gh-ac"))
+)
+search_box.send_keys("iphone 14")
+search_box.submit()
+
+time.sleep(5)  # wait for the page to load
+
+prices = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "s-item__price"))
+)
+
+for price in prices:
+    price_text = price.text
+    if len(price_text) > 1:
+        print(price_text)
+        send_to_slack(price_text)
+
+driver.quit()
